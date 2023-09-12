19c901cbc16f5ef6241c4447e05a9e6df1ce8244
UmerIftikhar111 <chumeriftikhar11@gmail.com>
2023-09-12 11:25:53 +0500

    1st commit

---
+++ app/src/main/java/com/example/androidapp/ItemDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ItemDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_details);
+    }
+}
