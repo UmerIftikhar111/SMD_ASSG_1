6a20ff7db65db45228397643d4e21a839092ab00
UmerIftikhar111 <chumeriftikhar11@gmail.com>
2023-10-13 20:55:02 +0500

    corrected more code to change pics (profile/cover) more

---
+++ app/src/main/java/comumer/i200784/Ad_item.java
@@ -0,0 +1,14 @@
+package comumer.i200784;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ad_item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ad_item);
+    }
+}
