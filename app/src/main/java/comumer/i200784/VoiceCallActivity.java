202d73142568b46d7bc288a51c8e1d5ed23ae02a
Revolt-47 <114021105+Revolt-47@users.noreply.github.com>
2023-10-13 16:13:51 +0500

    forgot password done

--- app/src/main/java/comumer/i200784/VoiceCallActivity.java
+++ app/src/main/java/comumer/i200784/VoiceCallActivity.java
@@ -3,15 +3,27 @@ package comumer.i200784;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class VoiceCallActivity extends AppCompatActivity {
 
+    TextView Caller , time;
+    ImageView mute , speaker , hold, cancel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.voice_call);
+
+
+        Caller = findViewById(R.id.caller_name);
+        time = findViewById(R.id.time);
+        mute = findViewById(R.id.mute);
+        speaker = findViewById(R.id.speaker);
+        hold = findViewById(R.id.hold);
+        cancel = findViewById(R.id.end_voice_call);
 
         // nav to chat details
         ImageView endCallIcn = findViewById(R.id.end_voice_call);
