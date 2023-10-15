69c0d13c10702760e1b0faefcef7bab2a87dbb84
Revolt-47 <114021105+Revolt-47@users.noreply.github.com>
2023-10-15 16:13:08 +0500

    voice call implemented

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,15 +45,15 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation ("com.squareup.picasso:picasso:2.71828")
-    implementation ("androidx.camera:camera-core:1.2.2")
-    implementation ("androidx.camera:camera-camera2:1.2.3")
-    implementation ("androidx.camera:camera-lifecycle:1.2.3")
-    implementation ("androidx.camera:camera-view:1.2.3")
-    implementation ("androidx.camera:camera-extensions:1.2.3")
-    implementation ("androidx.camera:camera-viewfinder:1.4.0-alpha01")
-    implementation ("androidx.camera:camera-video:1.2.3")
-
-
-
+    implementation("com.squareup.picasso:picasso:2.71828")
+    implementation("androidx.camera:camera-core:1.2.2")
+    implementation("androidx.camera:camera-camera2:1.2.3")
+    implementation("androidx.camera:camera-lifecycle:1.2.3")
+    implementation("androidx.camera:camera-view:1.2.3")
+    implementation("androidx.camera:camera-extensions:1.2.3")
+    implementation("androidx.camera:camera-viewfinder:1.4.0-alpha01")
+    implementation("androidx.camera:camera-video:1.2.3")
+    implementation ("io.agora.rtc:voice-sdk:4.0.0.4")
 }
+
+
--- app/src/main/java/comumer/i200784/VoiceCallActivity.java
+++ app/src/main/java/comumer/i200784/VoiceCallActivity.java
@@ -1,36 +1,55 @@
 package comumer.i200784;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+
+
 
 public class VoiceCallActivity extends AppCompatActivity {
 
     TextView Caller , time;
-    ImageView mute , speaker , hold, cancel;
+    public ImageView mute , speaker , hold, cancel;
 
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.voice_call);
 
 
-        Caller = findViewById(R.id.caller_name);
-        time = findViewById(R.id.time);
-        mute = findViewById(R.id.mute);
-        speaker = findViewById(R.id.speaker);
-        hold = findViewById(R.id.hold);
-        cancel = findViewById(R.id.end_voice_call);
 
-        // nav to chat details
-        ImageView endCallIcn = findViewById(R.id.end_voice_call);
-        endCallIcn.setOnClickListener(view -> {
-            Intent intent = new Intent(VoiceCallActivity.this, ChatDetailsActivity.class);
-            startActivity(intent);
-        });
+
+
+        TextView name = findViewById(R.id.name);
+        time = findViewById(R.id.time);
+        cancel = findViewById(R.id.leave);
+
+
+
+
+
+//        Caller = findViewById(R.id.caller_name);
+//        time = findViewById(R.id.time);
+//        mute = findViewById(R.id.mute);
+//        speaker = findViewById(R.id.speaker);
+//        hold = findViewById(R.id.hold);
+//        cancel = findViewById(R.id.end_voice_call);
+
+
 
     }
+
+
+
 }
